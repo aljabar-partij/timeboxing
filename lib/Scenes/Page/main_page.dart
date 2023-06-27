7fd8988bf3290ab491cbe1f909d4c45831d30eec
irfandigalih <60963651+irfandigalih@users.noreply.github.com>
2023-06-27 14:29:11 +0700

    add remove debug banner and bottom nav bar and resolve conflict

--- lib/Scenes/Page/main_page.dart
+++ lib/Scenes/Page/main_page.dart
@@ -6,6 +6,28 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: Colors.black,
+          unselectedItemColor: Colors.red,
+          items: const [
+              BottomNavigationBarItem(
+                label: 'Home',
+                icon: Icon(Icons.home, color: Colors.black)
+              ),
+              BottomNavigationBarItem(
+                label: 'Settings',
+                icon: Icon(Icons.calendar_month, color: Colors.black)
+              ),
+              BottomNavigationBarItem(
+                label: 'Create',
+                icon: Icon(Icons.create, color: Colors.black)
+              )
+              // BottomNavigationBarItem(
+              //   label: 'Love', 
+              //   icon: Icon(Icons.favorite, color: Colors.black)
+              // )
+          ],
+        ),
       body: Container(
         alignment: Alignment.center,
         child: Text('Jordy Pinix Kecil Kontolnya'),
