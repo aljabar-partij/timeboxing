0e68d147e1b32db75545b859433044889a325c85
irfandigalih <60963651+irfandigalih@users.noreply.github.com>
2023-07-12 22:24:17 +0700

    [Home] Numero uno task list modello

--- lib/Shared/Widget/TaskList/Component/task_list.dart
+++ lib/Shared/Widget/TaskList/Component/task_list.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:timeboxing/Shared/Extension/colors_style_extension.dart';
 import 'package:timeboxing/Shared/Extension/icons_style_extension.dart';
 import 'package:timeboxing/Shared/Extension/text_style_extension.dart';
+import 'package:timeboxing/Shared/Widget/TaskList/Model/task_item_model.dart';
 
 class TaskList extends StatefulWidget {
   const TaskList({super.key});
@@ -11,30 +12,38 @@ class TaskList extends StatefulWidget {
 }
 
 class _TimeboxingTodayTaskState extends State<TaskList> {
+  final TaskItem _taskItem = TaskItem(
+    description: 'Ini Jordy gatau',
+    id: '12',
+    name: 'Mengerjakan Tugas apa',
+    taskPriority: TaskPriority(type: TaskPriorityType.p0),
+    time: '08.00',
+  );
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 16),
       child: IntrinsicHeight(
         child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Flexible(
               flex: 2,
               child: Container(
                 padding: const EdgeInsets.all(10),
                 decoration: BoxDecoration(
-                  color: TimeBoxingColors.neutralBlack(),
+                  color: _taskItem.taskPriority.color,
                   borderRadius: BorderRadius.circular(4),
                 ),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text('P',
+                    Text(_taskItem.taskPriority.label ?? '',
                         style: TimeBoxingTextStyle.headline2(
                           TimeBoxingFontWeight.bold,
                           TimeBoxingColors.neutralWhite(),
                         )),
-                    Text('conditional',
+                    Text(_taskItem.taskPriority.name ?? '',
                         style: TimeBoxingTextStyle.paragraph5(
                           TimeBoxingFontWeight.bold,
                           TimeBoxingColors.neutralWhite(),
@@ -43,14 +52,13 @@ class _TimeboxingTodayTaskState extends State<TaskList> {
                 ),
               ),
             ),
-            const Spacer(
-              flex: 1,
-            ),
-            const Expanded(
+            Expanded(
               flex: 8,
               child: Column(
                 children: [
-                  TaskItem(),
+                  Task(taskItem: _taskItem),
+                  Task(taskItem: _taskItem),
+                  Task(taskItem: _taskItem),
                 ],
               ),
             ),
@@ -61,20 +69,20 @@ class _TimeboxingTodayTaskState extends State<TaskList> {
   }
 }
 
-class TaskItem extends StatefulWidget {
-  const TaskItem({super.key});
-
+class Task extends StatefulWidget {
+  const Task({super.key, required this.taskItem});
+  final TaskItem taskItem;
   @override
-  State<TaskItem> createState() => _MyWidgetState();
+  State<Task> createState() => _MyWidgetState();
 }
 
-class _MyWidgetState extends State<TaskItem> {
+class _MyWidgetState extends State<Task> {
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Row(
             children: [
@@ -90,13 +98,13 @@ class _MyWidgetState extends State<TaskItem> {
                   child: Row(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text('Task',
+                      Text(widget.taskItem.name,
                           style: TimeBoxingTextStyle.paragraph3(
                             TimeBoxingFontWeight.regular,
                             TimeBoxingColors.text(TimeBoxingColorType.tint),
                           )),
                       const Spacer(),
-                      Text('Time',
+                      Text(widget.taskItem.time,
                           style: TimeBoxingTextStyle.paragraph3(
                             TimeBoxingFontWeight.bold,
                             TimeBoxingColors.text(TimeBoxingColorType.tint),
@@ -117,7 +125,7 @@ class _MyWidgetState extends State<TaskItem> {
           const SizedBox(
             height: 4,
           ),
-          Text('Description',
+          Text(widget.taskItem.description,
               style: TimeBoxingTextStyle.paragraph4(
                 TimeBoxingFontWeight.regular,
                 TimeBoxingColors.text40(TimeBoxingColorType.tint),
