cfb8fdfd308cd6df03f64bdc1abbdd74e0c9f338
irfandigalih <60963651+irfandigalih@users.noreply.github.com>
2023-07-12 18:39:47 +0700

    Update today_task.dart

--- lib/Scenes/Page/HomePage/Component/today_task.dart
+++ lib/Scenes/Page/HomePage/Component/today_task.dart
@@ -66,7 +66,7 @@ class _TimeboxingTodayTaskState extends State<TimeboxingTodayTask> {
                     )),
               ),
             ),
-            TaskList(),
+            const TaskList(),
           ],
         ));
   }
