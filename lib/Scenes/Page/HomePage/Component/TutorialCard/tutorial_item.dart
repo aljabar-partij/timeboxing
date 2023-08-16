93256a1eb67bd4a204eed09e1e5dc4af18657a97
irfandigalih <60963651+irfandigalih@users.noreply.github.com>
2023-08-16 21:12:25 +0700

    tutorial design

--- lib/Scenes/Page/HomePage/Component/TutorialCard/tutorial_item.dart
+++ lib/Scenes/Page/HomePage/Component/TutorialCard/tutorial_item.dart
@@ -13,63 +13,34 @@ class TutorialItem extends StatefulWidget {
 class _MyWidgetState extends State<TutorialItem> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(bottom: 12),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Expanded(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  height: 75,
-                  decoration: BoxDecoration(
-                    color: TimeBoxingColors.primary50(TimeBoxingColorType.tint),
-                    borderRadius: const BorderRadius.all(Radius.circular(4)),
-                  ),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(4),
-                    child: Image.network(
-                        fit: BoxFit.fill, widget.tutorialModel.image),
-                  ),
-                ),
-                const SizedBox(
-                  width: 24,
-                ),
-                Flexible(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        widget.tutorialModel.tittle,
-                        style: TimeBoxingTextStyle.paragraph2(
-                          TimeBoxingFontWeight.bold,
-                          TimeBoxingColors.text(TimeBoxingColorType.shade),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 12,
-                      ),
-                      Text(
-                        widget.tutorialModel.description,
-                        style: TimeBoxingTextStyle.paragraph3(
-                          TimeBoxingFontWeight.regular,
-                          TimeBoxingColors.text30(TimeBoxingColorType.tint),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.only(right: 12),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(4),
+              child: Image.network(
+                widget.tutorialModel.image,
+              ),
             ),
-          ),
-          Icon(
-            Icons.chevron_right,
-            size: 16,
-            color: TimeBoxingColors.primary40(TimeBoxingColorType.shade),
-          )
-        ],
+            Text(
+              widget.tutorialModel.tittle,
+              style: TimeBoxingTextStyle.paragraph2(
+                TimeBoxingFontWeight.bold,
+                TimeBoxingColors.neutralBlack(),
+              ),
+            ),
+            Text(
+              widget.tutorialModel.description,
+              style: TimeBoxingTextStyle.paragraph3(
+                TimeBoxingFontWeight.regular,
+                TimeBoxingColors.neutralBlack(),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
