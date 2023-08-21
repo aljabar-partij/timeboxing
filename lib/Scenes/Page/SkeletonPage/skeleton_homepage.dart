8370791dd290a71dd28a5930d27ea743174ddc60
irfandigalih <60963651+irfandigalih@users.noreply.github.com>
2023-08-22 01:09:49 +0700

    Update skeleton_homepage.dart

--- lib/Scenes/Page/SkeletonPage/skeleton_homepage.dart
+++ lib/Scenes/Page/SkeletonPage/skeleton_homepage.dart
@@ -13,15 +13,84 @@ class _MyWidgetState extends State<SkeletonPage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: TimeBoxingColors.neutralWhite(),
+        backgroundColor: TimeBoxingColors.neutralLotion(),
         body: Column(
           children: [
             Container(
               padding: const EdgeInsets.fromLTRB(24, 56, 24, 12),
-              child: Row(
+              child: Expanded(
+                child: Row(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color: TimeBoxingColors.text90(
+                                TimeBoxingColorType.tint),
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(8)),
+                          ),
+                          width: 106,
+                          height: 18,
+                        ),
+                        const SizedBox(
+                          height: 4,
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: TimeBoxingColors.text90(
+                                TimeBoxingColorType.tint),
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(8)),
+                          ),
+                          width: 140,
+                          height: 18,
+                        ),
+                      ],
+                    ),
+                    const Spacer(),
+                    Container(
+                      decoration: BoxDecoration(
+                          color:
+                              TimeBoxingColors.text90(TimeBoxingColorType.tint),
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(30))),
+                      height: 24,
+                      width: 24,
+                    ),
+                    const SizedBox(
+                      width: 12,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                          color:
+                              TimeBoxingColors.text90(TimeBoxingColorType.tint),
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(30))),
+                      height: 24,
+                      width: 24,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: TimeBoxingColors.neutralWhite(),
+                borderRadius: const BorderRadius.all(Radius.circular(16)),
+                boxShadow: [
+                  BoxShadow(
+                    offset: const Offset(0, 0),
+                    color: TimeBoxingColors.neutralBlack().withOpacity(0.08),
+                    blurRadius: 8,
+                  ),
+                ],
+              ),
+              padding: const EdgeInsets.all(24),
+              child: Column(
                 children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  Row(
                     children: [
                       Container(
                         decoration: BoxDecoration(
@@ -30,11 +99,11 @@ class _MyWidgetState extends State<SkeletonPage> {
                           borderRadius:
                               const BorderRadius.all(Radius.circular(8)),
                         ),
-                        width: 106,
+                        width: 52,
                         height: 18,
                       ),
                       const SizedBox(
-                        height: 4,
+                        width: 16,
                       ),
                       Container(
                         decoration: BoxDecoration(
@@ -43,11 +112,199 @@ class _MyWidgetState extends State<SkeletonPage> {
                           borderRadius:
                               const BorderRadius.all(Radius.circular(8)),
                         ),
-                        width: 140,
+                        width: 52,
                         height: 18,
                       ),
                     ],
-                  )
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Center(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color:
+                            TimeBoxingColors.text90(TimeBoxingColorType.tint),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(8)),
+                      ),
+                      width: 120,
+                      height: 20,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color:
+                              TimeBoxingColors.text90(TimeBoxingColorType.tint),
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(8)),
+                        ),
+                        width: 56,
+                        height: 142,
+                      ),
+                      Column(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              color: TimeBoxingColors.text90(
+                                  TimeBoxingColorType.tint),
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(8)),
+                            ),
+                            width: 284,
+                            height: 32,
+                          ),
+                          const SizedBox(
+                            height: 6,
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                              color: TimeBoxingColors.text90(
+                                  TimeBoxingColorType.tint),
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(8)),
+                            ),
+                            width: 284,
+                            height: 32,
+                          ),
+                          const SizedBox(
+                            height: 6,
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                              color: TimeBoxingColors.text90(
+                                  TimeBoxingColorType.tint),
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(8)),
+                            ),
+                            width: 284,
+                            height: 32,
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color:
+                              TimeBoxingColors.text90(TimeBoxingColorType.tint),
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(8)),
+                        ),
+                        width: 56,
+                        height: 142,
+                      ),
+                      Column(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              color: TimeBoxingColors.text90(
+                                  TimeBoxingColorType.tint),
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(8)),
+                            ),
+                            width: 284,
+                            height: 32,
+                          ),
+                          const SizedBox(
+                            height: 6,
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                              color: TimeBoxingColors.text90(
+                                  TimeBoxingColorType.tint),
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(8)),
+                            ),
+                            width: 284,
+                            height: 32,
+                          ),
+                          const SizedBox(
+                            height: 6,
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                              color: TimeBoxingColors.text90(
+                                  TimeBoxingColorType.tint),
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(8)),
+                            ),
+                            width: 284,
+                            height: 32,
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 22),
+              padding: const EdgeInsets.all(24),
+              decoration: BoxDecoration(
+                color: TimeBoxingColors.neutralWhite(),
+                borderRadius: const BorderRadius.all(Radius.circular(16)),
+                boxShadow: [
+                  BoxShadow(
+                    offset: const Offset(0, 0),
+                    color: TimeBoxingColors.neutralBlack().withOpacity(0.08),
+                    blurRadius: 8,
+                  ),
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color:
+                              TimeBoxingColors.text90(TimeBoxingColorType.tint),
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(8)),
+                        ),
+                        width: 186,
+                        height: 20,
+                      ),
+                      const Spacer(),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: TimeBoxingColors.text90(TimeBoxingColorType.tint),
+                      borderRadius: const BorderRadius.all(Radius.circular(8)),
+                    ),
+                    width: 382,
+                    height: 128,
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: TimeBoxingColors.text90(TimeBoxingColorType.tint),
+                      borderRadius: const BorderRadius.all(Radius.circular(8)),
+                    ),
+                    width: 382,
+                    height: 36,
+                  ),
                 ],
               ),
             )
