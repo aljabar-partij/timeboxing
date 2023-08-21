9c34c83ad0f916af3f161247237444e8b89589ad
irfandigalih <60963651+irfandigalih@users.noreply.github.com>
2023-08-21 20:57:25 +0700

    Update skeleton_homepage.dart

--- lib/Scenes/Page/SkeletonPage/skeleton_homepage.dart
+++ lib/Scenes/Page/SkeletonPage/skeleton_homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:timeboxing/Shared/Extension/colors_style_extension.dart';
 
 class SkeletonPage extends StatefulWidget {
   const SkeletonPage({super.key});
@@ -10,6 +11,49 @@ class SkeletonPage extends StatefulWidget {
 class _MyWidgetState extends State<SkeletonPage> {
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: TimeBoxingColors.neutralWhite(),
+        body: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.fromLTRB(24, 56, 24, 12),
+              child: Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color:
+                              TimeBoxingColors.text90(TimeBoxingColorType.tint),
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(8)),
+                        ),
+                        width: 106,
+                        height: 18,
+                      ),
+                      const SizedBox(
+                        height: 4,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          color:
+                              TimeBoxingColors.text90(TimeBoxingColorType.tint),
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(8)),
+                        ),
+                        width: 140,
+                        height: 18,
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
