4b9718846f605749138f9907a83c2d45c5e6a6b8
irfandigalih <60963651+irfandigalih@users.noreply.github.com>
2023-08-29 21:11:34 +0700

    Update notification_page.dart

--- lib/Scenes/Page/NotificationPage/notification_page.dart
+++ lib/Scenes/Page/NotificationPage/notification_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:timeboxing/Scenes/Page/HomePage/Component/InvitationCard/invitation_card.dart';
+import 'package:timeboxing/Shared/Extension/colors_style_extension.dart';
 
 class NotificationPage extends StatefulWidget {
   const NotificationPage({super.key});
@@ -11,16 +12,21 @@ class NotificationPage extends StatefulWidget {
 class _MyWidgetState extends State<NotificationPage> {
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-        child: SingleChildScrollView(
-            child: Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          TimeboxingInvitationCard(),
-        ],
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              const TimeboxingInvitationCard(),
+              ListTile(
+                title: const Text('data'),
+                tileColor: TimeBoxingColors.rainbow1(),
+                hoverColor: Colors.black,
+              ),
+            ],
+          ),
+        ),
       ),
-    )));
+    );
   }
 }
